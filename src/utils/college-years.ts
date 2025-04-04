e83ab52f7b59a0d80e98464ae4fe3e7ff4a0008f
Wederson Fagundes <wedymf@hotmail.com>
2025-04-04 18:39:41 -0300

    feat: add utilities

---
+++ src/utils/college-years.ts
@@ -0,0 +1,13 @@
+export const COLLEGE_YEARS: Record<string, string> = {
+  "0": "Educacao Infantil",
+  "1": "Primeiro Ano",
+  "2": "Segundo Ano",
+  "3": "Terceiro Ano",
+  "4": "Quarto Ano",
+  "5": "Quinto Ano",
+  "6": "Sexto Ano",
+  "7": "Setimo Ano",
+  "8": "Oitavo Ano",
+  "9": "Nono Ano",
+  "69": "Sexto Ao Nono"
+} as const
---
+++ src/utils/menu.ts
@@ -0,0 +1,23 @@
+
+import { BookA, Brain, Home } from "lucide-react"
+
+export const items = [
+  {
+    title: "Início",
+    url: "/",
+    icon: Home,
+    shown: true,
+  },
+  {
+    title: "Habilidades",
+    url: "/habilidades",
+    icon: Brain,
+    shown: true,
+  },
+  {
+    title: "Detalhes",
+    url: "/detalhes",
+    icon: BookA,
+    shown: false,
+  }
+]
