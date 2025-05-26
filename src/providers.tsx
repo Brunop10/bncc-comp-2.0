99b5dca0ebe10962363c80e0a11f1fe44ab05e44
Wederson Fagundes <wedymf@hotmail.com>
2025-05-26 04:28:06 -0300

    feat: add react query

--- src/providers.tsx
+++ src/providers.tsx
@@ -1,11 +1,15 @@
 import { RouterProvider } from "react-router";
 import { router } from "./router";
 import { DataProvider } from "./contexts/data-provider";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query";
 
 export function Providers() {
   return (
-    <DataProvider>
-      <RouterProvider router={router} />
-    </DataProvider>
+    <QueryClientProvider client={queryClient}>
+      <DataProvider>
+        <RouterProvider router={router} />
+      </DataProvider>
+    </QueryClientProvider>
   )
 }
