04ea397f1aa2c725575168fd804b7dc75181dd91
Wederson Fagundes <wedymf@hotmail.com>
2025-05-26 04:27:19 -0300

    feat: add tw v3 + dayjs

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
+import '@/index.css'
+import '@/lib/dayjs.ts'
+
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import { Providers } from './providers.tsx'
 
 createRoot(document.getElementById('root')!).render(<Providers />)
