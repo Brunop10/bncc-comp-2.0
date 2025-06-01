7a6b8278227baa84a6eeb43df6f6e555e4ed617f
Wederson Fagundes <wedymf@hotmail.com>
2025-06-01 20:48:27 -0300

    feat: show toast messages

--- src/providers.tsx
+++ src/providers.tsx
@@ -2,11 +2,15 @@ import { RouterProvider } from "react-router";
 import { router } from "./router";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/react-query";
+import { Toaster } from "./components/ui/sonner";
 
 export function Providers() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+      <Toaster richColors theme="light" />
+    </>
   )
 }
